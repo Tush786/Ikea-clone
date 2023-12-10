b27f656c2b4047c69d1e7802e72a72258545202c
Ashutosh Kumawat <aurecordsofficial@gmail.com>
2023-12-10 17:55:24 +0530

    first commit

--- frontend/src/home/AllRoutes.jsx
+++ frontend/src/home/AllRoutes.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Home from './Home'
+import { Route, Routes } from 'react-router-dom'
+
 
 function AllRoutes() {
   return (
     <div>
-      
+         <Routes>
+        <Route path='/'element={<Home/>}/>
+        
+    </Routes>
     </div>
   )
 }
---
+++ frontend/src/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sec1 from './Secs/Sec1'
+import Sec2 from './Secs/Sec2'
+import Sec3 from './Secs/Sec3'
+
+const Home = () => {
+  return (
+    <div>
+        <Sec1/>
+        <Sec2/>
+        <Sec3/>
+    </div>
+  )
+}
+
+export default Home
