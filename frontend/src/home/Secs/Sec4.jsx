dbf4ba81b5f3ac10c727b204d5c264c511dd4fa4
Ashutosh Kumawat <aurecordsofficial@gmail.com>
2023-12-11 22:05:18 +0530

    swiper added

--- frontend/src/home/Secs/Sec4.jsx
+++ frontend/src/home/Secs/Sec4.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Sec1 = () => {
+const Sec4 = () => {
   return (
     <div>
         <h2 className='flex justify-center font-semibold text-3xl mt-4'>
-        Design deals, delivered free
+        Explore each unique collection
         </h2>
     </div>
   )
 }
 
-export default Sec1
+export default Sec4
