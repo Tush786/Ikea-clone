5ef243030b1fc55f20ce253dc909df6f93d6b029
karnking <karankotai9183@gmail.com>
2023-12-18 23:27:55 +0530

    feat: admni login

--- frontend/src/admin/AdminRoute.jsx
+++ frontend/src/admin/AdminRoute.jsx
@@ -1,17 +1,49 @@
 import { useState } from 'react'
-import {Modal, ModalBody, useDisclosure} from '@chakra-ui/react'
+import { Modal, HStack, VStack, FormControl, Button, ModalFooter, FormLabel, Input, PinInput, PinInputField, ModalOverlay, ModalContent, ModalHeader, ModalBody, useDisclosure } from '@chakra-ui/react'
+import AdminPage from "./AdminPage.jsx"
 
-const AdminRoute = ({ children }) => {
-    const [logged,setLogged] = useState(false)
-    const {isOpen,onOpen,onClose} = useDisclosure()
-    if(!logged) return <Modal isOpen={logged} onOpen={onOpen} onClose={onClose}>
-        <ModalBody></ModalBody>
+const AdminRoute = () => {
+    const [logged, setLogged] = useState(false)
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [adminId,setAdminId] = useState("")
+    const [passcode,setPasscode] = useState("")
+    if (!logged) return <Modal isOpen={!logged} onOpen={onOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent className='text-center'>
+            <ModalHeader className='font-bold text-center bg-slate-700 text-white rounded-t-md'>
+                Admin Login
+            </ModalHeader>
+            <ModalBody className='pt-2'>
+                <form className='pt-5'>
+                    <VStack className='flex-col gap-4 p-2'>
+                        <FormControl isRequired>
+                            <HStack className='flex'>
+                                <FormLabel className='w-2/6'>Admin Id</FormLabel>
+                                <Input placeholder='Basic usage' />
+                            </HStack>
+                        </FormControl>
+                        <FormControl isRequired>
+                            <HStack className='flex'>
+                                <FormLabel className='w-1/3'>PassCode</FormLabel>
+                                <HStack>
+                                    <PinInput type='alphanumeric' mask>
+                                        <PinInputField />
+                                        <PinInputField />
+                                        <PinInputField />
+                                        <PinInputField />
+                                    </PinInput>
+                                </HStack>
+                            </HStack>
+                        </FormControl>
+                    </VStack>
+                </form>
+            </ModalBody>
+            <ModalFooter>
+                <Button bg={'#334155'} w='full' color='white'>Login</Button>
+            </ModalFooter>
+        </ModalContent>
     </Modal>
-    return (
-        <>
-            
-        </>
-    )
+    return <AdminPage />
 }
 
 export default AdminRoute
--- frontend/src/admin/RightPanel.jsx
+++ frontend/src/admin/RightPanel.jsx
@@ -4,14 +4,15 @@ import Products from './Products'
 import AddProducts from './AddProducts'
 import UsersComp from './UsersComp'
 import Dashboard from './dashboard/Dashboard'
+import ErrorPage from './ErrorPage'
 
 const RightPanel = ({ tab }) => {
     return tab === "dashboard" ? <Dashboard />
-    : tab === "orders" ? <Orders />
-    : tab === "products" ? <Products />
-    : tab === "addProduct" ? <AddProducts />
-    : tab === "users" ? <UsersComp />
-    : <></>
+        : tab === "orders" ? <Orders />
+            : tab === "products" ? <Products />
+                : tab === "addProduct" ? <AddProducts />
+                    : tab === "users" ? <UsersComp />
+                        : <ErrorPage />
 }
 
 export default RightPanel
