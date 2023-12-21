21e42a935b7efd31e5c0b53484d1fa7a223968eb
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2023-12-21 23:06:16 +0530

    backend card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import Navbar from './home/Navbar'
 import AllRoutes from './home/AllRoutes'
 import AdminPage from './admin/AdminPage'
 import Productpage from './product/Productpage'
-import Carts from './cart/Carts'
 
 function App() {
   return (
