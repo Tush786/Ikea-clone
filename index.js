015b788d309a889ee956a46dacc46ae9e98f05de
Tush786 <tusharsapate34@gmail.com>
2024-08-23 21:34:26 +0530

    env inde file and gitignore move in root folder

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { connection } = require("./backend/config/connection");
+const { productRouter } = require("./backend/routerLinks/product.route");
+const { UserRouter } = require("./backend/routerLinks/userroute");
+const { Authentication } = require("./backend/middlwere/Authentication");
+const { Forgetpassrouter } = require("./backend/routerLinks/Forgetpassword");
+const { Cartrouter } = require("./backend/routerLinks/Cartrouter");
+const { Addressroute } = require("./backend/routerLinks/address.route");
+const cors = require("cors");
+const { OrderRoute } = require("./backend/routerLinks/order.route");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
+app.use(express.json());
+
+// Registering routes
+app.use("/productsapi", productRouter);
+app.use("/user", UserRouter);
+app.use('/cart', Authentication, Cartrouter);
+app.use('/pass', Forgetpassrouter);
+app.use('/address', Authentication, Addressroute);
+app.use('/order', Authentication, OrderRoute);
+
+// Setting up the server port
+const PORT = process.env.PORT || 9090;
+
+app.listen(PORT, async () => {
+    try {
+        // Ensure your connection function is properly set up
+        await connection; // if connection is a promise, otherwise remove `await`
+        console.log("Database connection successful");
+    } catch (err) {
+        console.error("Failed to connect to the database:", err);
+    }
+    console.log(`Server is connected to port ${PORT}`);
+});
